5a38b79231a7dcd3cde06ef61f6cb5624d75ca9a
task4233 <tks.m1205@gmail.com>
2020-08-08 10:42:37 +0900

    config for built environment (#3)
    * add config for built environment
    
    * update .gitignore
    
    * fix entrypoint.sh

---
+++ .github/actions/godoc-action/entrypoint.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+set -e
+
+# -------------
+# Environments
+# -------------
+
+RUN=$1
+WORKING_DIR=$2
+GITHUB_TOKEN=$3
+COMMENT=""
+SUCCESS=0
+
+# if not set, assign default value
+if [ "$2" = "" ]; then
+    WORKING_DIR="kadai2/task4233/eimg"
+fi
+
+cd ${WORKING_DIR}
+DOCNAME="doc"
+PKGNAME=$(go list ./...)
+
+# ------------
+# Functions
+# ------------
+
+init() {
+    # make directory setting for gh-pages
+    echo "make directory setting for gh-pages"
+    rm -rf ./${DOCNAME}
+    mkdir ./${DOCNAME}
+    
+    # install config file for layout
+    echo "install config file for layout"
+    cp ${GOPATH}/pkg/mod/golang.org/x/tools@*/godoc/static/jquery.js ./${DOCNAME}/
+    cp ${GOPATH}/pkg/mod/golang.org/x/tools@*/godoc/static/godocs.js ./${DOCNAME}/
+    cp ${GOPATH}/pkg/mod/golang.org/x/tools@*/godoc/static/style.css ./${DOCNAME}/
+}
+
+generate_godoc() {
+    echo "url: http://localhost:8080/pkg/github.com/task4233/gopherdojo-studyroom/${WORKING_DIR}"
+    godoc -url "http://localhost:8080/pkg/github.com/task4233/gopherdojo-studyroom/${WORKING_DIR}" | sed -e '1d' | sed 's/\/lib\/godoc/\./g' > ./${DOCNAME}/index.html
+}
+
+# -------------
+# Main
+# ------------
+init
+generate_godoc
+
+
+exit ${SUCCESS}
+
